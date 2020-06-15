499b46ac642d28f07fc9d4160bbf23172b67d541
caixuanbin <caixuanbin_1988@126.com>
2020-06-15 22:19:23 +0800

    java8新特性

---
+++ src/test/java/com/xbcai/java8demo/Java8DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xbcai.java8demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
